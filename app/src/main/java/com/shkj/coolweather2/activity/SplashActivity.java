64aa041e04b7209e48d0228f48bb08d59753cfc3
ypk <2908363622@qq.com>
2017-07-18 11:30:49 +0800

    first commit

---
+++ app/src/main/java/com/shkj/coolweather2/activity/SplashActivity.java
@@ -0,0 +1,27 @@
+package com.shkj.coolweather2.activity;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.shkj.coolweather2.R;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashActivity.this, MainActivity.class));
+                finish();
+            }
+        }, 3 * 1000);
+    }
+}
+
