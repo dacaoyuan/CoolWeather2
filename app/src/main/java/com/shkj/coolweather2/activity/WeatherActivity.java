b203bb63c8b6d07a2f0054a848693ad357d01b64
ypk <2908363622@qq.com>
2017-07-22 09:28:55 +0800

    修复状态栏的问题

--- app/src/main/java/com/shkj/coolweather2/activity/WeatherActivity.java
+++ app/src/main/java/com/shkj/coolweather2/activity/WeatherActivity.java
@@ -74,20 +74,15 @@ public class WeatherActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        if (Build.VERSION.SDK_INT >= 21) {
-            View decorView = getWindow().getDecorView();
-            decorView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_FULLSCREEN | View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
-            getWindow().setStatusBarColor(Color.TRANSPARENT);
-        }
 
         //透明状态栏效果
-        /*if (Build.VERSION.SDK_INT >= 21) {
+        if (Build.VERSION.SDK_INT >= 21) {
             View decorView = getWindow().getDecorView();
             int option = View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN | View.SYSTEM_UI_FLAG_LAYOUT_STABLE;
             decorView.setSystemUiVisibility(option);
             getWindow().setStatusBarColor(Color.TRANSPARENT);
 
-        }*/
+        }
 
         setContentView(R.layout.activity_weather);
         ButterKnife.bind(this);
